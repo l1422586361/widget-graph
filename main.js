b9f81e37665e9c09c4887f452d578368db0c6424
lin <1422586361@qq.com>
2022-12-02 17:44:11 +0800

    思源加载时自动生成初始图，简化扩展代码

--- main.js
+++ main.js
@@ -347,146 +347,13 @@ $(function () {
                 addNode(graph, id, desc);
                 break;
             case "扩展一度关系笔记":
-                (async function () {
-                    let nodes = []
-                    let edges = []
-                    let res1 = await getAllLinks(id)
-                    for (let e1 of res1) { // 第一层
-                        setArrToJson(e1, nodes, edges)
-                    }
-                    console.log(nodes, edges)
-                    let data = graph.save()
-                    let allNodes = data.nodes.map(e => { return { id: e.id } })
-                    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
-                    // console.log(122, allNodes, allEdges)
-                    for (let node of nodes) {
-                        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
-                            // console.log(allNodes.map(e=>{return e.id}),node.id)
-                            allNodes.push(node)
-                        }
-                    }
-                    for (let edge of edges) {
-                        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
-                            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
-                            allEdges.push(edge)
-                        }
-                    }
-                    data.nodes = allNodes; data.edges = allEdges
-                    console.log(data)
-                    graph.changeData(data)
-                })()
+                expand1LayerOfRelationship(id, graph)
                 break;
             case "扩展二度关系笔记":
-                (async function () {
-                    let nodes = []
-                    let edges = []
-                    let res1 = await getAllLinks(id)
-                    for (let e1 of res1) { // 第一层
-                        setArrToJson(e1, nodes, edges)
-                        if (e1.sourceId != id) {
-                            let res2 = await getAllLinks(e1.sourceId)
-                            for (let e2 of res2) { // 第二层
-                                setArrToJson(e2, nodes, edges)
-                            }
-                        }
-                        if (e1.targetId != id) {
-                            let res2 = await getAllLinks(e1.targetId)
-                            for (let e2 of res2) { // 第二层
-                                setArrToJson(e2, nodes, edges)
-                            }
-                        }
-                    }
-                    console.log(nodes, edges)
-                    let data = graph.save()
-                    let allNodes = data.nodes.map(e => { return { id: e.id } })
-                    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
-                    // console.log(122, allNodes, allEdges)
-                    for (let node of nodes) {
-                        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
-                            // console.log(allNodes.map(e=>{return e.id}),node.id)
-                            allNodes.push(node)
-                        }
-                    }
-                    for (let edge of edges) {
-                        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
-                            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
-                            allEdges.push(edge)
-                        }
-                    }
-                    data.nodes = allNodes; data.edges = allEdges
-                    console.log(data)
-                    graph.changeData(data)
-                })()
-
-
-
+                expand2LayerOfRelationship(id, graph)
                 break;
             case "扩展三度关系笔记":
-                (async function () {
-                    let nodes = []
-                    let edges = []
-                    let res1 = await getAllLinks(id)
-                    for (let e1 of res1) {  // 第一层
-                        setArrToJson(e1, nodes, edges)
-                        if (e1.sourceId != id) {
-                            let res2 = await getAllLinks(e1.sourceId)
-                            for (let e2 of res2) { // 第二层
-                                setArrToJson(e2, nodes, edges)
-                                if (e2.sourceId != e1.sourceId) {
-                                    let res3 = await getAllLinks(e2.sourceId)
-                                    for (let e3 of res3) { // 第三层
-                                        setArrToJson(e3, nodes, edges)
-                                    }
-                                }
-                                if (e2.targetId != e1.sourceId) {
-                                    let res3 = await getAllLinks(e2.targetId)
-                                    for (let e3 of res3) { // 第三层
-                                        setArrToJson(e3, nodes, edges)
-                                    }
-                                }
-                            }
-                        }
-                        if (e1.targetId != id) {
-                            let res2 = await getAllLinks(e1.targetId)
-                            for (let e2 of res2) {  // 第二层
-                                setArrToJson(e2, nodes, edges)
-                                if (e2.sourceId != e1.targetId) {
-                                    let res3 = await getAllLinks(e2.sourceId)
-                                    for (let e3 of res3) { // 第三层
-                                        setArrToJson(e3, nodes, edges)
-                                    }
-                                }
-                                if (e2.targetId != e1.targetId) {
-                                    let res3 = await getAllLinks(e2.targetId)
-                                    for (let e3 of res3) { // 第三层
-                                        setArrToJson(e3, nodes, edges)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    console.log(nodes, edges)
-                    let data = graph.save()
-                    let allNodes = data.nodes.map(e => { return { id: e.id } })
-                    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
-                    console.log(122, allNodes, allEdges)
-                    for (let node of nodes) {
-                        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
-                            // console.log(allNodes.map(e=>{return e.id}),node.id)
-                            allNodes.push(node)
-                        }
-                    }
-                    for (let edge of edges) {
-                        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
-                            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
-                            allEdges.push(edge)
-                        }
-                    }
-                    data.nodes = allNodes; data.edges = allEdges
-                    console.log(data)
-                    graph.changeData(data)
-                })()
-
+                expand3LayerOfRelationship(id, graph)
                 break;
         }
         // getBackLinkNode(graph,id)
@@ -496,7 +363,7 @@ $(function () {
 
 
 
-export function setArrToJson(arr, nodes, edges) {
+function setArrToJson(arr, nodes, edges) {
     let node1 = { id: arr.sourceId, label: arr.sourceDesc }
     let node2 = { id: arr.targetId, label: arr.targetDesc }
     let edge = { source: arr.sourceId, target: arr.targetId }
@@ -625,4 +492,166 @@ $(function () {
     $("#tab0").click(function () {
         $('#rightNav').css("display", "none")
     })
+
 })
+
+
+
+
+
+
+// 扩展笔记关系
+
+async function expand1LayerOfRelationship(id, obj) {
+    // 扩展一层关系渲染到图里
+    let nodes = []
+    let edges = []
+    let res1 = await getAllLinks(id)
+    for (let e1 of res1) { // 第一层
+        setArrToJson(e1, nodes, edges)
+    }
+    console.log(nodes, edges)
+    let data = obj.save()
+    let allNodes = data.nodes.map(e => { return { id: e.id } })
+    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
+    // console.log(122, allNodes, allEdges)
+    for (let node of nodes) {
+        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
+            // console.log(allNodes.map(e=>{return e.id}),node.id)
+            allNodes.push(node)
+        }
+    }
+    for (let edge of edges) {
+        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
+            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
+            allEdges.push(edge)
+        }
+    }
+    data.nodes = allNodes; data.edges = allEdges
+    console.log(data)
+    obj.changeData(data)
+}
+
+
+async function expand2LayerOfRelationship(id, obj) {
+    let nodes = []
+    let edges = []
+    let res1 = await getAllLinks(id)
+    for (let e1 of res1) { // 第一层
+        setArrToJson(e1, nodes, edges)
+        if (e1.sourceId != id) {
+            let res2 = await getAllLinks(e1.sourceId)
+            for (let e2 of res2) { // 第二层
+                setArrToJson(e2, nodes, edges)
+            }
+        }
+        if (e1.targetId != id) {
+            let res2 = await getAllLinks(e1.targetId)
+            for (let e2 of res2) { // 第二层
+                setArrToJson(e2, nodes, edges)
+            }
+        }
+    }
+    console.log(nodes, edges)
+    let data = obj.save()
+    let allNodes = data.nodes.map(e => { return { id: e.id } })
+    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
+    // console.log(122, allNodes, allEdges)
+    for (let node of nodes) {
+        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
+            // console.log(allNodes.map(e=>{return e.id}),node.id)
+            allNodes.push(node)
+        }
+    }
+    for (let edge of edges) {
+        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
+            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
+            allEdges.push(edge)
+        }
+    }
+    data.nodes = allNodes; data.edges = allEdges
+    console.log(data)
+    obj.changeData(data)
+}
+
+
+async function expand3LayerOfRelationship(id, obj) {
+    let nodes = []
+    let edges = []
+    let res1 = await getAllLinks(id)
+    for (let e1 of res1) {  // 第一层
+        setArrToJson(e1, nodes, edges)
+        if (e1.sourceId != id) {
+            let res2 = await getAllLinks(e1.sourceId)
+            for (let e2 of res2) { // 第二层
+                setArrToJson(e2, nodes, edges)
+                if (e2.sourceId != e1.sourceId) {
+                    let res3 = await getAllLinks(e2.sourceId)
+                    for (let e3 of res3) { // 第三层
+                        setArrToJson(e3, nodes, edges)
+                    }
+                }
+                if (e2.targetId != e1.sourceId) {
+                    let res3 = await getAllLinks(e2.targetId)
+                    for (let e3 of res3) { // 第三层
+                        setArrToJson(e3, nodes, edges)
+                    }
+                }
+            }
+        }
+        if (e1.targetId != id) {
+            let res2 = await getAllLinks(e1.targetId)
+            for (let e2 of res2) {  // 第二层
+                setArrToJson(e2, nodes, edges)
+                if (e2.sourceId != e1.targetId) {
+                    let res3 = await getAllLinks(e2.sourceId)
+                    for (let e3 of res3) { // 第三层
+                        setArrToJson(e3, nodes, edges)
+                    }
+                }
+                if (e2.targetId != e1.targetId) {
+                    let res3 = await getAllLinks(e2.targetId)
+                    for (let e3 of res3) { // 第三层
+                        setArrToJson(e3, nodes, edges)
+                    }
+                }
+            }
+        }
+    }
+    console.log(nodes, edges)
+    let data = obj.save()
+    let allNodes = data.nodes.map(e => { return { id: e.id } })
+    let allEdges = data.edges.map(e => { return { source: e.source, target: e.target } })
+    console.log(122, allNodes, allEdges)
+    for (let node of nodes) {
+        if (allNodes.map(e => { return e.id }).indexOf(node.id) === -1) {
+            // console.log(allNodes.map(e=>{return e.id}),node.id)
+            allNodes.push(node)
+        }
+    }
+    for (let edge of edges) {
+        if (allEdges.map(e => { return JSON.stringify({ source: e.source, target: e.target }) }).indexOf(JSON.stringify({ source: edge.source, target: edge.target })) === -1) {
+            // console.log(allEdges.map(e=>{return { source: e.source, target: e.target }}),{source: edge.source, target: edge.target })
+            allEdges.push(edge)
+        }
+    }
+    data.nodes = allNodes; data.edges = allEdges
+    console.log(data)
+    obj.changeData(data)
+}
+
+
+
+window.onload = function () {
+    try {
+        setTimeout(function () {
+            let nodeid = window.frameElement.parentElement.parentElement.dataset.nodeId
+            getBlockByID(nodeid).then(e => {
+                expand1LayerOfRelationship(e.root_id, graph)
+            })
+        }, 2000) // 延时，需要等待挂件块id入库，不然getBlockByID查不到数据
+    } catch (err) {
+        console.warn(err);
+        console.log("当前不在思源文档内部")
+    }
+}
